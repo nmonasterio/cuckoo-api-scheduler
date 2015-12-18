c730910dbc9d1d5290557a2675f63a7c7f07b0a6
nmonasterio <napo.monasterio@acsal.com>
2015-12-18 16:33:41 -0600

    Adding common files.

---
+++ CuckooCommon/JobScheduler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace CuckooCommon
+{
+    public class JobScheduler
+    {
+        public class EmailJob : IJob
+        {
+            public void Execute(IJobExecutionContext context)
+            {
+                using (var message = new MailMessage("test@acsal.com", "nmonasterio@acsal.com"))
+                {
+
+                    JobDataMap dataMap = context.MergedJobDataMap;
+
+                    var superStuff = (Amazingers)dataMap.Where(d => d.Key == "superstuff").First().Value;
+
+                    message.Subject = dataMap.GetString("name");
+                    message.Body = "Woot";
+                    using (SmtpClient client = new SmtpClient
+                    {
+                        Host = "relay.ntdomain.lan"
+                        
+                    })
+                    {
+                        client.Send(message);
+                    }
+                }
+            }
+        }
+
+
+        public static void StartScheduler()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+            scheduler.Start();
+
+            var jobs = Jobs.GetAllJobs();
+
+            var super = new Amazingers();
+            super.SuperValue = "Wahoo";
+
+            IJobDetail job = JobBuilder.Create<EmailJob>().Build();
+
+            var jobData = new JobDataMap();
+            jobData.Add("superstuff", super);
+            jobData.Add("name", "foo");
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInSeconds(30)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                  ).UsingJobData(jobData)
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+        }
+    }
+
+    public class Amazingers
+    {
+        public string SuperValue { get; set; } 
+    }
+}
---
+++ CuckooCommon/Jobs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace CuckooCommon
+{
+    public class Jobs
+    {
+        public ObjectId JobId { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Name { get; set;  }
+        public string Description { get; set; }
+
+        private static void createJobsDatabase()
+        {
+            const string connectionString = "mongodb://localhost";
+            var mongo = new MongoClient(connectionString);
+            
+
+            var blog = mongo.GetDatabase("Cuckoo");
+
+        }
+
+        public static IMongoCollection<Jobs> GetAllJobs()
+        {
+            //createJobsDatabase();
+
+            const string connectionString = "mongodb://localhost";
+
+            var mongo = new MongoClient(connectionString);
+            //var dbs = mongo.ListDatabases();
+
+            //var x = dbs.ToList();
+
+            var db = mongo.GetDatabase("Cuckoo");
+
+            var jobs = db.GetCollection<Jobs>("jobs");
+
+            jobs.InsertOne(new Jobs
+                {
+                    StartTime = DateTime.Now,
+                    Name = "Test",
+                    Description = "Amazingness"
+                });
+
+            return jobs;
+        }
+
+    }
+}
